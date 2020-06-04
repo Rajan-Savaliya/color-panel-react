4985f6c120a315a1a465827433014b64d4c3e45b
rajan savaliya <rajanpatel99094@gmail.com>
2020-06-04 11:38:40 +0530

    Updates

---
+++ precache-manifest.f950f655d5409a4533416821d826d8c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "51888adbe8c54758e008",
+    "url": "/color-panel-react/static/css/main.5e599100.chunk.css"
+  },
+  {
+    "revision": "51888adbe8c54758e008",
+    "url": "/color-panel-react/static/js/main.e4bb5fee.chunk.js"
+  },
+  {
+    "revision": "2bb804fd49c421b151e8",
+    "url": "/color-panel-react/static/js/runtime~main.1aaa860a.js"
+  },
+  {
+    "revision": "385c414adc3eccc4d904",
+    "url": "/color-panel-react/static/css/2.97ada392.chunk.css"
+  },
+  {
+    "revision": "385c414adc3eccc4d904",
+    "url": "/color-panel-react/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/color-panel-react/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "6abdcdab625273e96160b421d5b8d4c8",
+    "url": "/color-panel-react/index.html"
+  }
+];
